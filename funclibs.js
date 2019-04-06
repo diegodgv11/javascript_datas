f0e6d4689c15da7768c5ffdcb788f394483272be
diegodgv11 <diegevelascomuy@gmail.com>
2019-04-06 17:49:58 -0600

    Create funclibs.js

---
+++ funclibs.js
@@ -0,0 +1,28 @@
+String.prototype.format = function(...values) {
+	cadena = this;
+	for(let i in values){
+		cadena = cadena.replace(`{${i}}`, values[i])
+	}
+	return cadena
+};
+
+function ide(x){
+	return document.getElementById(x);
+}
+
+function cle(x){
+	return document.getElementsByClassName(x);
+}
+
+function loaded(x){
+	window.addEventListener("load",x);
+}
+
+function getAll(){
+	var all = document.body.getElementsByTagName('*');
+	for(let i of all){
+		if (i.id != ""){
+			eval(`${i.id} = document.getElementById('${i.id}')`);
+		}
+	}
+}	
